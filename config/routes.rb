4911ca94b99a10b6d636576b81c09377158e6d4c
rsnaka <166614766+rsnaka@users.noreply.github.com>
2024-06-23 13:47:13 +0000

    [strat]Bookers2_modelファイルにアソシエーションの記載

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  get 'users/edit'
+  
+  resources :books, only: [:new, :index, :show, :edit]
+  
+  get 'homes/top'
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
