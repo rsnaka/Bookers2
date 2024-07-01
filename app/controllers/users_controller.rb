eab2369c1107fdb346b84ae49dcd1d0478bea448
rsnaka <166614766+rsnaka@users.noreply.github.com>
2024-07-01 14:52:04 +0000

    [strat]Bookers2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
-  def show_params
+  def show
+    @user = User.find(params[:id])
+  end
+  
+  def user_params
     params.require(:user).permit(:image, :name, :introduction)
     @user = Uers.all
   end
