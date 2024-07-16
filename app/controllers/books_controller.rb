e5526a628f15f93957bdc27176dd62fb4edd82e7
rsnaka <166614766+rsnaka@users.noreply.github.com>
2024-07-16 15:14:13 +0000

    応用Bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -16,6 +16,7 @@ class BooksController < ApplicationController
   def index
     @book = Book.new
     @books = Book.all
+    @user = @book.user
   end
 
   def show
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,11 @@
 class UsersController < ApplicationController
   def index
-    @user = User.all
+    @user = User.find(params[:id])
   end
 
   def show
     @user = User.find(params[:id])
+    @user = @book.user
     @books = @user.books
   end
 
